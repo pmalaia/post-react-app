9d0887eb3ebb9464213f0608f9f904028f564200
Полина Малая <p.malaya@ngrsoftlab.ru>
2022-03-28 19:02:33 +0300

    added loader and fetchhook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import PostFilter from "./components/PostFilter";
@@ -7,23 +7,30 @@ import "./styles/App.css";
 import CommonModal from "./components/UI/modal/CommonModal";
 import CommonButton from "./components/UI/button/CommonButton";
 import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
-  const [posts, setPosts] = useState([
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
 
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
-
-
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(false)
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -32,18 +39,32 @@ function App() {
 
   return (
     <div className="App">
-      <CommonButton style={{marginTop: '15px'}} onClick={() => setModal(true)}>Create post</CommonButton>
+      <CommonButton
+        style={{ marginTop: "15px" }}
+        onClick={() => setModal(true)}
+      >
+        Create post
+      </CommonButton>
       <CommonModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </CommonModal>
 
       <hr />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Posts list"
-      />
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+        >
+          <Loader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Posts list"
+        />
+      )}
     </div>
   );
 }
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,35 +1,36 @@
-import CommonInput from "./UI/input/CommonInput"
-import CommonButton from "./UI/button/CommonButton"
-import { useState } from "react"
+import CommonInput from "./UI/input/CommonInput";
+import CommonButton from "./UI/button/CommonButton";
+import { useState } from "react";
 
-const PostForm = ({create}) => {
-    const [post, setPost] = useState({title: '', body: ''})
+const PostForm = ({ create }) => {
+  const [post, setPost] = useState({ title: "", body: "" });
 
-    const addNewPost = (e) => {
-        e.preventDefault()
-        const newPost = {
-            ...post, id: Date.now()
-        }
-        create(newPost)
-        setPost({    title: '', body: ''})
-      }
-      return (
-      <form>
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      ...post,
+      id: Date.now(),
+    };
+    create(newPost);
+    setPost({ title: "", body: "" });
+  };
+  return (
+    <form>
       <CommonInput
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-          type="text"
-          placeholder="Post name"
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+        type="text"
+        placeholder="Post name"
       />
       <CommonInput
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-          type="text"
-          placeholder="Description"
+        value={post.body}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
+        type="text"
+        placeholder="Description"
       />
       <CommonButton onClick={addNewPost}>Create post</CommonButton>
-  </form>
-      )
-}
+    </form>
+  );
+};
 
 export default PostForm;
